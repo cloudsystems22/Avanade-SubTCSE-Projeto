c84dbb8f16058e83577ee0a8d4dc1d2762f84ad5
cloudsystems22 <cloudsystems22@gmail.com>
2021-06-21 09:18:24 -0300

    Implementations and automapper

---
+++ src/Avanade.SubTCSE.Projeto.Application/AutoMapperConfigs/Profiles/EmployeeRole/EmployeeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Avanade.SubTCSE.Projeto.Application.AutoMapperConfigs.Profiles.EmployeeRole
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<Dtos.EmployeeRole.EmployeeRoleDto, Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
+                .ForCtorParam("roleName", opt => opt.MapFrom(src => src.Cargo));
+
+            CreateMap<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, Dtos.EmployeeRole.EmployeeRoleDto>()
+               .ForMember("roleCargo", opt => opt.MapFrom(src => src.RoleName));
+        }
+    }
+}
---
+++ src/Avanade.SubTCSE.Projeto.Application/Services/EmployeeRole/EmployeeRoleAppServices.cs
@@ -0,0 +1,14 @@
+﻿using Avanade.SubTCSE.Projeto.Application.Dtos.EmployeeRole;
+using Avanade.SubTCSE.Projeto.Application.Interfaces.EmployeeRole;
+
+namespace Avanade.SubTCSE.Projeto.Application.Services.EmployeeRole
+{
+    public class EmployeeRoleAppServices : IEmployeeRoleAppServices
+    {
+        void IEmployeeRoleAppServices.addEmployeeRole(EmployeeRoleDto employeeRoleDto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
