3b68c8bceeb010751cab04199fa7d1b84fa5dbaf
cloudsystems22 <cloudsystems22@gmail.com>
2021-07-05 08:53:31 -0300

    Implementation mongodb and interfaces

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Repositories/IEmployeeRoleRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Repositories/IEmployeeRoleRepository.cs
@@ -1,7 +1,9 @@
-﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Repository
+﻿using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Repository
 {
-    public interface IEmployeeRoleRepository
+    public interface IEmployeeRoleRepository : IBaseRepository<Entities.EmployeeRole, string>
     {
-        void addEmployeeRole(Entities.EmployeeRole employeeRole);
+        
     }
 }
